08f733327d9ac5d5288a4a2cc7ef0b0fab3a4c1d
elenareboud <elena.reboud@gmail.com>
2024-01-30 14:39:58 +0100

    add seed TierHasCampaign

--- config/initializeDb.js
+++ config/initializeDb.js
@@ -55,15 +55,6 @@ initializeDatabase();
     //on ajoute nos fausses données
 
         
-    await Exchange.create({ UserId: 1, ContactId: 1, content: 'envoyer un devis' })
-    await Exchange.create({ UserId: 2, ContactId: 2, content: 'rapeller plus tard.' })
-    await Exchange.create({ UserId: 3, ContactId: 3, content: 'ne plus déranger.' })
-    await Exchange.create({ UserId: 7, ContactId: 5, content: 'prise de contact ok' })
-    await Exchange.create({ UserId: 7, ContactId: 5, content: "invitation a un rdv en attente d'une réponse" })
-    await Exchange.create({ UserId: 7, ContactId: 5, content: 'retour a la case départ' })
-    console.log("données EXCHANGE ok");
-
-    
     await TierHasCampaign.create({ TierId: 1, CampaignId:1 })
     await TierHasCampaign.create({ TierId: 2, CampaignId:2 })
     await TierHasCampaign.create({ TierId: 3, CampaignId:3 })
---
+++ seeds/seedTierHasCampaign.js
@@ -0,0 +1,24 @@
+import TierHasCampaign from "../models/authorisation.js";
+
+const seedTierHasCampaign = async () => {
+  try {
+    await TierHasCampaign.create({ TierId: 1, CampaignId:1 })
+    await TierHasCampaign.create({ TierId: 2, CampaignId:2 })
+    await TierHasCampaign.create({ TierId: 3, CampaignId:3 })
+    await TierHasCampaign.create({ TierId: 4, CampaignId:4 })
+    await TierHasCampaign.create({ TierId: 5, CampaignId:5 })
+    await TierHasCampaign.create({ TierId: 6, CampaignId:6 })
+    await TierHasCampaign.create({ TierId: 5, CampaignId:6 })
+    await TierHasCampaign.create({ TierId: 4, CampaignId:6 })
+    await TierHasCampaign.create({ TierId: 3, CampaignId:6 })
+    await TierHasCampaign.create({ TierId: 2, CampaignId:6 })
+    await TierHasCampaign.create({ TierId: 1, CampaignId:6 })
+
+    console.log("données TIER/CAMPAIGN ok**********");
+  
+  } catch (error) {
+    console.error('Erreur lors du seeding de TIER/CAMPAIGN:', error);
+    }
+  };
+
+export default seedTierHasCampaign;
