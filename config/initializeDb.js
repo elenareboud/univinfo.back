6abfa0f41c4c2d0f1e304f428b671a42b4c6ba04
elenareboud <elena.reboud@gmail.com>
2024-01-30 14:33:26 +0100

    add seedCampaign

--- config/initializeDb.js
+++ config/initializeDb.js
@@ -61,47 +61,7 @@ initializeDatabase();
     console.log('données AUTHORISATION ok')
     // Seed des données pour la table CONTACT
     
-
-    await Campaign.create({
-        name: "Solde d'été",
-        description: "Promotion pour les produit d'été",
-        type: 'Email',
-        message: 'venez voir nos ventes exclusifs!',
-
-    })
-    await Campaign.create({
-        name: 'lancement nouveau produit',
-        description: "introduction d'un nouveau produit dans notre gamme",
-        type: 'Phoning',
-        message: 'Exciting news! We have just launched our new product.',
-
-    })
-    await Campaign.create({
-        name: 'Nouveauté',
-        description: 'Nouveau magasin',
-        type: 'Email',
-        message: 'Nous ouvrons un nouveau magasin !',
-    })
-    await Campaign.create({
-        name: 'Jeu video',
-        description: 'Offre de lancement',
-        type: 'Phoning',
-        message: 'Nouveau produit à découvrir.',
-    })
-    await Campaign.create({
-        name: 'Journée spéciale',
-        description: 'Soldes sur tout',
-        type: 'Email',
-        message: 'Venez profiter de nos soldes sur tous les produits !',
-    })
-    await Campaign.create({
-        name: 'nouveau CRM',
-        description: 'présentation application',
-        type: 'Email',
-        message: 'Venez découvrir notre super crm ',
-    })
-    console.log("données CAMPAIGN ok")
-
+    
     await Exchange.create({ UserId: 1, ContactId: 1, content: 'envoyer un devis' })
     await Exchange.create({ UserId: 2, ContactId: 2, content: 'rapeller plus tard.' })
     await Exchange.create({ UserId: 3, ContactId: 3, content: 'ne plus déranger.' })
---
+++ seeds/seedCampaign.js
@@ -0,0 +1,50 @@
+import Campaign from "../models/campaign.js";
+
+const seedCampaign = async () => {
+  try {
+    await Campaign.create({
+      name: "Solde d'été",
+      description: "Promotion pour les produit d'été",
+      type: 'Email',
+      message: 'venez voir nos ventes exclusifs!',
+
+  })
+  await Campaign.create({
+      name: 'lancement nouveau produit',
+      description: "introduction d'un nouveau produit dans notre gamme",
+      type: 'Phoning',
+      message: 'Exciting news! We have just launched our new product.',
+
+  })
+  await Campaign.create({
+      name: 'Nouveauté',
+      description: 'Nouveau magasin',
+      type: 'Email',
+      message: 'Nous ouvrons un nouveau magasin !',
+  })
+  await Campaign.create({
+      name: 'Jeu video',
+      description: 'Offre de lancement',
+      type: 'Phoning',
+      message: 'Nouveau produit à découvrir.',
+  })
+  await Campaign.create({
+      name: 'Journée spéciale',
+      description: 'Soldes sur tout',
+      type: 'Email',
+      message: 'Venez profiter de nos soldes sur tous les produits !',
+  })
+  await Campaign.create({
+      name: 'nouveau CRM',
+      description: 'présentation application',
+      type: 'Email',
+      message: 'Venez découvrir notre super crm ',
+  })
+  console.log("données CAMPAIGN ok")
+  
+  } catch (error) {
+    console.error('Erreur lors du seeding de CAMPAIGN', error);
+    }
+  };
+
+export default seedCampaign;
