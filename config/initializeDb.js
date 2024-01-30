5eb0f06ff2269efab34e104b3bf0271091853043
elenareboud <elena.reboud@gmail.com>
2024-01-30 14:45:43 +0100

    add seedUserHasCampaign.js

--- config/initializeDb.js
+++ config/initializeDb.js
@@ -55,26 +55,6 @@ initializeDatabase();
     //on ajoute nos fausses données
 
         
-    await TierHasCampaign.create({ TierId: 1, CampaignId:1 })
-    await TierHasCampaign.create({ TierId: 2, CampaignId:2 })
-    await TierHasCampaign.create({ TierId: 3, CampaignId:3 })
-    await TierHasCampaign.create({ TierId: 4, CampaignId:4 })
-    await TierHasCampaign.create({ TierId: 5, CampaignId:5 })
-    await TierHasCampaign.create({ TierId: 6, CampaignId:6 })
-    await TierHasCampaign.create({ TierId: 5, CampaignId:6 })
-    await TierHasCampaign.create({ TierId: 4, CampaignId:6 })
-    await TierHasCampaign.create({ TierId: 3, CampaignId:6 })
-    await TierHasCampaign.create({ TierId: 2, CampaignId:6 })
-    await TierHasCampaign.create({ TierId: 1, CampaignId:6 })
-    console.log("********affectation TIER/campaign ok**********");
     
-    await UserHasCampaign.create({ UserId: 1, CampaignId:1 })
-    await UserHasCampaign.create({ UserId: 2, CampaignId:2 })
-    await UserHasCampaign.create({ UserId: 3, CampaignId:3 })
-    console.log("********affectation USER/campaign ok**********");
-
-} catch (error) {
-    console.error('Il y a un probléme', error);
-}
 
 
---
+++ seeds/seedUserHasCampaign.js
@@ -0,0 +1,15 @@
+import UserHasCampaign from "../models/authorisation.js";
+
+const seedUserHasCampaign = async () => {
+  try {
+    await UserHasCampaign.create({ UserId: 1, CampaignId:1 })
+    await UserHasCampaign.create({ UserId: 2, CampaignId:2 })
+    await UserHasCampaign.create({ UserId: 3, CampaignId:3 })
+
+    console.log('données USER/CAMPAIGN ok')
+  } catch (error) {
+    console.error('Erreur lors du seeding de USER/CAMPAIGN:', error);
+    }
+  };
+
+export default seedUserHasCampaign;
