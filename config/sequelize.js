2f4d5bb74f3c75893117074f99555097877d2e2c
elenareboud <elena.reboud@gmail.com>
2024-02-12 14:24:01 +0100

    deploy2

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,23 +1,32 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { Sequelize } from "sequelize";
-//créer et configurer une instance de .sequelize
-//const sequelize = new Sequelize('database', 'username', 'password', {....
+ //creer et configurer une instance de .sequelize
+ //const sequelize = new Seuqlize('database', 'username', 'password', {})
 
-
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER,  
-    process.env.DB_PASSWORD, 
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
-        //logging: false,
-        define: {
-            underscored: true
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: process.env.NODE_ENV === 'production' ? {
+            require: true,
+            rejectUnauthorized: false
+        } : false
     }
-} 
-);
+});
+
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME, 
+//     process.env.DB_USER,  
+//     process.env.DB_PASSWORD, 
+//     {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialect: process.env.DB_DIALECT,
+//         logging: false,
+//         define: {
+//             underscored: true
+//     }
+// });
 
 export default sequelize;
